71bb89159c3e1620de177ff0b0ce5833439c2077
Mohammed-Saheem <pimohammedsahu72@gmail.com>
2025-08-17 16:25:09 +0530

    random color updater

---
+++ app.js
@@ -0,0 +1,18 @@
+let btn=document.querySelector("button");
+btn.addEventListener("click",function(){
+    let randomColor=getRandomColor()
+    let h1=document.querySelector("h1");
+    h1.innerText=randomColor;
+    h1.style.color=randomColor;
+    console.log("color updated");
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomColor;
+});
+function getRandomColor(){
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+
+    let color=`rgb(${red},${green},${blue})`;
+    return color;
+}
